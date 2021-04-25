2c0af2cd572b5a234c496cfb3423e48a63a632fc
U787020 <66324795+U787020@users.noreply.github.com>
2021-04-25 17:37:03 +0200

    Hi Lukas, ich hab mir jetzt den Kopf zerbrochen - es schaut für mich vollständig aus, aber die Liste sortiert nicht richtig :-( (Die Fragen sind im auskommentierten Bereich unten in der Main) Ich find nicht, wo es bei der Liste hakt.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,44 @@
 package application;
 
+import list.SortedLinkedList;
+import list.SortedList;
+import model.Animal;
+
 public class Main {
     public static void main(String[] args) {
        /*
             test your implementation here
         */
+        Animal x=new Animal("X",7);
+        Animal hund=new Animal("R",7);
+        Animal katze=new Animal("D",2);
+        Animal b=new Animal("B",7);
+        Animal kuh=new Animal("A",7);
+
+        SortedLinkedList<Animal>list=new SortedLinkedList<Animal>();
+        list.insert(x);
+        list.insert(b);
+        list.insert(hund);
+        list.insert(katze);
+        list.insert(kuh);
+
+        list.printList();
     }
 }
+
+/*
+QUESTIONS:
+1. Change the ordering of the list:
+Collections.sort
+Collections.reverse
+Implement Comparator as class and use "ascending"/"descending"
+2. Can the inner node be static?
+No. Only classes and instances can be static.
+3. Rename a generic placeholder?
+Possible, but doesn't make sense - "placeholder" is not a data type.
+4. Benefits of <T> data structure
+You don't need casts for type changes
+5. Drawbacks w/use-case
+a. Type erasure: instanceOf is not possible.
+b. Generics doesn't work with primitive data types -> Arrays are not possible
+ */
